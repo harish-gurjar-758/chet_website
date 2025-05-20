a373792ca9f59fa94004dbc405c36de9b189d26d
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-20 23:15:10 +0530

    ctreate and connect the mongoDb file and connect the MongoDb database in server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+import { connectDB } from './src/lib/db.js';
 import authRoutes from './src/routes/auth.route.js'
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, ()=>{
-    console.log("server is runint on post 5001")
+app.listen(PORT, () => {
+    console.log(`Server is runing on :-  http://localhost:${PORT}`);
+    connectDB();
 });
