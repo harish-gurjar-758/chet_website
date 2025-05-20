b1ca809fbd093cb9c3b0d1a59a39db505c2f29f6
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-20 23:40:30 +0530

    set the midle whare and create the signup controller

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import authRoutes from './src/routes/auth.route.js'
 dotenv.config();
 const app = express();
 
+// middleware 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
