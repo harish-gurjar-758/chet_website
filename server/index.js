f01e57c92da522b707d7a40100098bc71cddb41e
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-20 22:46:38 +0530

    setup the auth route file and connect the index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import authRoutes from './src/routes/auth.route.js'
+
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, ()=>{
     console.log("server is runint on post 5001")
