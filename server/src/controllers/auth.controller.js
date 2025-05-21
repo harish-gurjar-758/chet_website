2749dc52945bc87d56aa9854df1596158a54f12c
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-21 23:07:54 +0530

    create the login controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,9 +2,10 @@ import { generateToken } from '../lib/utils.js';
 import User from '../modules/user.model.js';
 import bcrypt from 'bcryptjs';
 
+// Sign Up Controller
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
-    
+
     try {
         // Validate input
         if (!fullName || !email || !password) {
@@ -50,9 +51,38 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Something went wrong. Please try again." }); // 🎯 Clean error message
     }
 };
-export const login = (req, res) => {
-    res.send("login route");
+
+// Sign In && Log In controller
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne(email);
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
+
+// Sign Out && Log Out Controller
 export const logout = (req, res) => {
     res.send("logout route");
 }
