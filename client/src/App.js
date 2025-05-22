6949d6af284114fa77c333844d624121631c77af
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-22 23:53:48 +0530

    setup the tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import React from 'react'
+import './index.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div className='text-3xl font-bold text-red-500 text-center'>
+      this is home opage
+      <p className='text-red-500'>hhbh</p>
     </div>
-  );
+  )
 }
-
-export default App;
