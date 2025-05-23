bee09cceed2ffbeab1d5c7bb161155515c61a0b3
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-23 20:13:59 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
-      <h1 className="text-5xl font-bold text-white">Welcome to Chet Website</h1>
     </div>
   );
 }
