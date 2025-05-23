a92d21d94735b0374493d3000d63819901627f66
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-23 20:10:39 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import './index.css'
+import React from "react";
 
-export default function App() {
+function App() {
   return (
-    <div className='text-3xl font-bold text-red-500 text-center'>
-      this is home opage
-      <p className='text-red-500'>hhbh</p>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-5xl font-bold text-white">Welcome to Chet Website</h1>
     </div>
-  )
+  );
 }
+
+export default App;
