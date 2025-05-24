dc222d9283bfd9bfeac7564559d90e0f752d052e
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-24 23:32:35 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
 import { Route, Routes } from 'react-router-dom';
+import HomePage from './Components/Pages/HomePage';
+import SignUpPage from './Components/Pages/SignUpPage';
+import LogInPage from './Components/Pages/LogInPage';
+import SettingsPage from './Components/Pages/SettingsPage';
+import ProfilePage from './Components/Pages/ProfilePage';
+import { useAuthStore } from './store/useAuthStore';
 
 function App() {
+  const {authUser} = useAuthStore();
 
   return (
     <div>
       <NavBar />
       <Routes>
-        <Route path='/' element={<>this is home page</>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LogInPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </div>
   );
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 export default function NavBar() {
+  const {authUser} = useAuthStore();
   return (
     <div>
       
