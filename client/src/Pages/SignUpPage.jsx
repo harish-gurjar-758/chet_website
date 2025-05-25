b5bbbf6292f52b94f42d4f3774c13dcbef942d25
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-25 23:55:45 +0530

    remove the bugs

--- client/src/Pages/SignUpPage.jsx
+++ client/src/Pages/SignUpPage.jsx
@@ -20,7 +20,7 @@ export default function SignUpPage() {
         if (!formData.email.trim()) return toast.error("Email is required");
         if (!/\S+@\S+.\S+/.test(formData.email)) return toast.error("Invalid email format");
         if (!formData.password.trim()) return toast.error("Password is required");
-        if (!formData.password.length < 6) return toast.error("Password must be at least 6 characters");
+        if (!formData.password.length > 6) return toast.error("Password must be at least 6 characters");
 
         return true
     };
