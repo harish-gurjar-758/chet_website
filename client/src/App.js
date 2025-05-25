0aaf542b7b9336cbebb9cd72efa8326b7d397c4b
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-05-25 22:39:03 +0530

    set the authections for routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import NavBar from './Components/NavBar';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './Components/Pages/HomePage';
 import SignUpPage from './Components/Pages/SignUpPage';
 import LogInPage from './Components/Pages/LogInPage';
 import SettingsPage from './Components/Pages/SettingsPage';
 import ProfilePage from './Components/Pages/ProfilePage';
 import { useAuthStore } from './store/useAuthStore';
+import { Loader } from 'lucide-react'
 
 function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -18,7 +19,7 @@ function App() {
 
   if (isCheckingAuth && !authUser) return (
     <div>
-      <p>Loading..</p>
+      <p><Loader /></p>
     </div>
   )
 
@@ -26,11 +27,35 @@ function App() {
     <div>
       <NavBar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LogInPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
+        <Route
+          path='/'
+          element={
+            authUser ? <HomePage /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path='/signup'
+          element={
+            !authUser ? <SignUpPage /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path='/login' element={
+            !authUser ? <LogInPage /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path='/settings'
+          element={
+            authUser ? <SettingsPage /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            authUser ? <ProfilePage /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
     </div>
   );
